a83b858d7dcf60c37c48559c1833c0a41f6d0950
Rashedul Alam <alam.rashedul@genweb2.com>
2022-03-21 12:48:48 +0600

    New py file add

---
+++ Test/delivery_schedule_local.py
@@ -0,0 +1,19 @@
+import time
+from select import select
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import keys
+driver=webdriver.Firefox(executable_path="F:\Selenium\geckodriver")
+driver.get("http://192.168.1.105:8060/web/database/selector")
+time.sleep(2)
+driver.find_element_by_xpath("html/body/div[1]/div/div[2]/a[3]").click()
+time.sleep(2)
+element=driver.find_element_by_id("login").send_keys("rafsansheikh@yahoo.com")
+element=driver.find_element_by_id("password").send_keys("@dminsccl")
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div/main/div/form/div[4]/button").click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/nav/div[2]/ul[1]/li[3]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[1]/li[2]/a/span").click()
+time.sleep(2)
