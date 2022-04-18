bff59c3fcc6239c23cbec70b43cddd2a7724898c
Rashedul Alam <alam.rashedul@genweb2.com>
2022-04-18 17:20:27 +0600

    new addition

---
+++ LIVE/commercial_nahid.py
@@ -0,0 +1,24 @@
+import time
+from select import select
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import keys
+driver=webdriver.Firefox(executable_path="F:\Selenium\geckodriver")
+driver.get("https://gen-bizbd.com/web/login")
+time.sleep(2)
+element=driver.find_element_by_id("login").send_keys("commercial@scclbd.com")
+element=driver.find_element_by_id("password").send_keys("sdfsdfds")
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div/main/div/form/div[3]/button").click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/nav/div[2]/ul[1]/li[3]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[4]/li[1]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[1]/li[3]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]").click()
+time.sleep(2)
