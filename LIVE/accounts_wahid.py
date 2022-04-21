555edea678d84ca762dbfff9dd89b5934935b808
Rashedul Alam <alam.rashedul@genweb2.com>
2022-04-21 16:56:27 +0600

    accounts portion add

---
+++ LIVE/accounts_wahid.py
@@ -0,0 +1,28 @@
+import time
+from select import select
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import keys
+driver=webdriver.Firefox(executable_path="F:\Selenium\geckodriver")
+driver.get("https://gen-bizbd.com/web/login")
+time.sleep(2)
+element=driver.find_element_by_id("login").send_keys("wahid@scclbd.com")
+element=driver.find_element_by_id("password").send_keys("dsfsdfsd")
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div/main/div/form/div[3]/button").click()
+time.sleep(7)
+driver.find_element_by_xpath("/html/body/nav/div[2]/ul[1]/li[3]/a/span").click()
+time.sleep(4)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[2]/li[5]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[2]/li[3]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[3]/ul[2]/li[4]/a/span").click()
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]").click()
+time.sleep(2)
